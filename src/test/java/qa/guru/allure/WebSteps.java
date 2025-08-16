a6d28f606ce7280363dbfe881f877c42c680cb15
pavelnikitin <pavelnikitin@>
2025-08-16 18:19:37 +0400

    added AttachmentsTest,LabelsTest

--- src/test/java/qa/guru/allure/WebSteps.java
+++ src/test/java/qa/guru/allure/WebSteps.java
@@ -1,7 +1,12 @@
 package qa.guru.allure;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.commands.TakeScreenshot;
+import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
@@ -35,6 +40,11 @@ public class WebSteps {
     @Step("Проверяем наличие Issue с номером")
     public void shouldSeeIssueWithNumber(int issue) {
         $(withText("#" + issue)).should(Condition.exist);
+    }
+
+    @Attachment(value = "Screenshot", type = "image/png", fileExtension = "png")
+    public byte[] takeScreenshot() {
+        return ((TakesScreenshot)WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
     }
 
 }
