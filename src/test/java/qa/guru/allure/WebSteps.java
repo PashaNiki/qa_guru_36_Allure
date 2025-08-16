a8f349f101b44830308d927e5d427a13995b49a0
pavelnikitin <pavelnikitin@>
2025-08-16 17:28:04 +0400

    added SelenideTest,StepsTest,WebSteps

---
+++ src/test/java/qa/guru/allure/WebSteps.java
@@ -0,0 +1,40 @@
+package qa.guru.allure;
+
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
+public class WebSteps {
+
+    @Step("Открыываем главную страницу")
+    public void openMainPage() {
+        open("https://github.com");
+    }
+
+    @Step("Ищем репозиторий {repo}")
+    public void searchForRepository(String repo) {
+        $(".search-input-container").click();
+        $("#query-builder-test").sendKeys(repo);
+        $("#query-builder-test").submit();
+    }
+
+    @Step("Кликаем по ссылке репозитория {repo}")
+    public void clickOnRepositoryLink(String repo) {
+        $(linkText(repo)).click();
+    }
+
+    @Step("Окртываем таб Issues")
+    public void openIssuesTab() {
+        $("#issues-tab").click();
+    }
+
+    @Step("Проверяем наличие Issue с номером")
+    public void shouldSeeIssueWithNumber(int issue) {
+        $(withText("#" + issue)).should(Condition.exist);
+    }
+
+}
